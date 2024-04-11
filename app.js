b2ca46c9869f6487502d8e877c20923c119b45f3
pamukr <paumukros@gmail.com>
2024-04-11 13:08:09 +0200

    api working

--- app.js
+++ app.js
@@ -12,23 +12,60 @@ const db = createClient({
     authToken: process.env.DB_TOKEN
 });
 
-await db.execute('DROP TABLE users');
-
-
+//await db.execute('DROP TABLE users');
 
 app.get('/', (req, res) => {
     //GET
     uwu = req.query.uwu;
-
-    //POST
-    uwu = req.body.uwu;
+    //Status
+    res.sendStatus(200);
     //String
     res.send(`Hello ${uwu}`);
-
     //JSON
     res.json({ message: `Hello ${uwu}` });
 }
 );
+
+app.get('/verifytag', async (req, res) => {
+    var tag = req.query.tag;
+
+    // Verifica que tag sea una cadena
+    if (typeof tag !== 'string') {
+        return res.status(400).json({ error: 'El parámetro tag debe ser una cadena' });
+    }
+
+    // Elimina espacios en blanco adicionales al principio y al final de la cadena
+    tag = tag.trim();
+
+    try {
+        // Check if tag exists in the database
+        const result = await db.execute(
+            {
+                sql: 'SELECT * FROM business WHERE tag = :tag',
+                args: { tag }
+            }
+        );
+
+        if (result.length > 0) {
+            res.json({ exists: true });
+        } else {
+            res.json({ exists: false });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Error en la base de datos', message: error.message });
+    }
+});
+
+
+
+app.post('/newbusiness', (req, res) => {
+    business = req.body.business;
+    tag = req.body.tag;
+    password = req.body.password;
+    email = req.body.email;
+    phone = req.body.phone;
+
+});
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
