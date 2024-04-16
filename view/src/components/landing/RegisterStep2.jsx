a18e872312087068e7b848d760dd80284d70f7cd
Guitogom <guillemtg04@gmail.com>
2024-04-16 13:22:13 +0200

    Repeat password feedback

--- view/src/components/landing/RegisterStep2.jsx
+++ view/src/components/landing/RegisterStep2.jsx
@@ -5,6 +5,7 @@ import { WarnSVG } from '../../assets/svg/WarnSVG'
 export function RegisterStep2({ setCurrentStep, setDivHeight, divHeight, setPassword, handleRegisterBusiness }) {
     const [passwordProvisional, setPasswordProvisional] = useState('')
     const [repeatPassword, setRepeatPassword] = useState('')
+    const [passwordMatch, setPasswordMatch] = useState(false)
 
 
     const handlePassword = (e) => {
@@ -23,12 +24,21 @@ export function RegisterStep2({ setCurrentStep, setDivHeight, divHeight, setPass
         }, 400)
     }
 
+    useEffect(() => {
+        if (passwordProvisional === repeatPassword && passwordProvisional.length > 0) {
+            setPasswordMatch(true)
+            setPassword(passwordProvisional)
+        } else {
+            setPasswordMatch(false)
+        }
+    }, [passwordProvisional, repeatPassword])
+
     return (
         <div className={`register-form ${divHeight}`}>
             <button className='register-back' onClick={handleGoBack}>Go Back</button>
             <input type="password" name="" id="password" placeholder='create a business password...' className='register-input' onInput={handlePassword} />
             <input type="password" name="" id="repeat-password" placeholder='repeat the business password...' className='register-input' onInput={handleRepeatPassword} />
-            <div className="password-feedback"><CheckSVG /><WarnSVG /></div>
+            <div className="password-feedback">{passwordMatch ? <CheckSVG /> : <WarnSVG />}</div>
             <button className='register-button' onClick={handleRegisterBusiness}>Next</button>
         </div>
     )
