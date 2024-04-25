5b3b9af4a1198e68943e7d6131254f13d6d79c36
pamukr <paumukros@gmail.com>
2024-04-25 12:21:48 +0200

    funciones separadas en otro archivo

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 import express from 'express';
-import dotenv from 'dotenv';
-import { createClient } from '@libsql/client';
 import cors from 'cors';
-import jwt from 'jsonwebtoken';
+import { verifyTag, newBusiness, getBusiness, verificarToken } from './functions.js';
 
 
 dotenv.config();
@@ -15,111 +13,6 @@ const db = createClient({
     url: "libsql://forkkies-tumse.turso.io",
     authToken: process.env.DB_TOKEN
 });
-
-async function verifyTag(tag) {
-    // Verifica que tag sea una cadena
-    if (typeof tag !== 'string') {
-        throw new Error('El parámetro tag debe ser una cadena');
-    }
-
-    // Elimina espacios en blanco adicionales al principio y al final de la cadena
-    tag = tag.trim();
-
-    try {
-        // Check if tag exists in the database
-        const result = await db.execute(
-            {
-                sql: 'SELECT * FROM business WHERE tag = :tag',
-                args: { tag }
-            }
-        );
-
-        return result.rows.length > 0;
-    } catch (error) {
-        console.error('Error en la base de datos:', error.message);
-        throw new Error('Error en la base de datos: ' + error.message);
-    }
-}
-
-
-async function newbusiness(business) {
-    var incorrect_field = [];
-    if (await verifyTag(business.tag)) {
-        incorrect_field.push("tag");
-    }
-    if (!business.name) {
-        incorrect_field.push("name");
-    }
-    if (!business.type) {
-        incorrect_field.push("type");
-    }
-    if (!business.tel) {
-        incorrect_field.push("phone");
-    }
-    if (!business.password) {
-        incorrect_field.push("password");
-    }
-
-    //Si no hay campos incorrectos, añadimos el negocio a la base de datos
-    if (incorrect_field.length === 0) {
-        try {
-            await db.execute(
-                {
-                    sql: 'INSERT INTO business (tag, name, type, tel, password) VALUES (:tag, :name, :type, :tel, :password)',
-                    args: business
-                }
-            );
-        } catch (error) {
-            throw new Error('Error en la base de datos: ' + error.message);
-        }
-    } else {
-        //Le devolvemos la array de campos incorrectos
-        throw new Error('Campos incorrectos: ' + incorrect_field);
-    }
-    //Se genera un jwt con el tag del negocio
-    const token = jwt.sign({ tag: business.tag }, process.env.JWT_SECRET);
-    console.log('Token generado funcion:', token)
-    return token;
-};
-
-async function getBusiness(tag) {
-    try {
-        const result = await db.execute(
-            {
-                sql: 'SELECT * FROM business WHERE tag = :tag',
-                args: { tag }
-            }
-        );
-
-        return result.rows[0];
-    } catch (error) {
-        console.error('Error en la base de datos:', error.message);
-        throw new Error('Error en la base de datos: ' + error.message);
-    }
-
-}
-
-function verificarToken(req, res, next) {
-    // Extraer el token del encabezado de la solicitud
-    const token = req.headers.authorization;
-
-    // Verificar si el token está presente
-    if (!token) {
-        return res.status(401).json({ mensaje: 'Token no proporcionado' });
-    }
-
-    try {
-        // Verificar el token y decodificar su contenido
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-        req.tag = decoded.tag;
-        // Continuar con la ejecución del siguiente middleware o controlador
-        next();
-    } catch (error) {
-        // Si ocurre un error al verificar el token, devolver un error de autenticación
-        return res.status(401).json({ mensaje: 'Token inválido' });
-    }
-}
 
 
 //Rutas
@@ -141,7 +34,7 @@ app.get('/verifytag', async (req, res) => {
 
 app.post('/newbusiness', async (req, res) => {
     try {
-        var token = await newbusiness(req.body);
+        var token = await newBusiness(req.body);
         console.log('Token generado:', token)
         res.status(200).json({ token: token });
     } catch (error) {
---
+++ functions.js
@@ -0,0 +1,116 @@
+import { createClient } from '@libsql/client';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Crear el cliente de la base de datos
+const db = createClient({
+    url: "libsql://forkkies-tumse.turso.io",
+    authToken: process.env.DB_TOKEN
+});
+
+export async function verifyTag(tag) {
+    // Verifica que tag sea una cadena
+    if (typeof tag !== 'string') {
+        throw new Error('El parámetro tag debe ser una cadena');
+    }
+
+    // Elimina espacios en blanco adicionales al principio y al final de la cadena
+    tag = tag.trim();
+
+    try {
+        // Check if tag exists in the database
+        const result = await db.execute(
+            {
+                sql: 'SELECT * FROM business WHERE tag = :tag',
+                args: { tag }
+            }
+        );
+
+        return result.rows.length > 0;
+    } catch (error) {
+        console.error('Error en la base de datos:', error.message);
+        throw new Error('Error en la base de datos: ' + error.message);
+    }
+}
+
+
+export async function newBusiness(business) {
+    var incorrect_field = [];
+    if (await verifyTag(business.tag)) {
+        incorrect_field.push("tag");
+    }
+    if (!business.name) {
+        incorrect_field.push("name");
+    }
+    if (!business.type) {
+        incorrect_field.push("type");
+    }
+    if (!business.tel) {
+        incorrect_field.push("phone");
+    }
+    if (!business.password) {
+        incorrect_field.push("password");
+    }
+
+    //Si no hay campos incorrectos, añadimos el negocio a la base de datos
+    if (incorrect_field.length === 0) {
+        try {
+            await db.execute(
+                {
+                    sql: 'INSERT INTO business (tag, name, type, tel, password) VALUES (:tag, :name, :type, :tel, :password)',
+                    args: business
+                }
+            );
+        } catch (error) {
+            throw new Error('Error en la base de datos: ' + error.message);
+        }
+    } else {
+        //Le devolvemos la array de campos incorrectos
+        throw new Error('Campos incorrectos: ' + incorrect_field);
+    }
+    //Se genera un jwt con el tag del negocio
+    const token = jwt.sign({ tag: business.tag }, process.env.JWT_SECRET);
+    console.log('Token generado funcion:', token)
+    return token;
+};
+
+export async function getBusiness(tag) {
+    try {
+        const result = await db.execute(
+            {
+                sql: 'SELECT * FROM business WHERE tag = :tag',
+                args: { tag }
+            }
+        );
+
+        return result.rows[0];
+    } catch (error) {
+        console.error('Error en la base de datos:', error.message);
+        throw new Error('Error en la base de datos: ' + error.message);
+    }
+
+}
+
+export function verificarToken(req, res, next) {
+    // Extraer el token del encabezado de la solicitud
+    const token = req.headers.authorization;
+
+    // Verificar si el token está presente
+    if (!token) {
+        return res.status(401).json({ mensaje: 'Token no proporcionado' });
+    }
+
+    try {
+        // Verificar el token y decodificar su contenido
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.tag = decoded.tag;
+        // Continuar con la ejecución del siguiente middleware o controlador
+        next();
+    } catch (error) {
+        // Si ocurre un error al verificar el token, devolver un error de autenticación
+        return res.status(401).json({ mensaje: 'Token inválido' });
+    }
+}
