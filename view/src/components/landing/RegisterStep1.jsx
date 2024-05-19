aedd08412724fef7d1a0779e093cb689d116913b
Guitogom <guillemtg04@gmail.com>
2024-05-19 13:26:44 +0200

    Registro muestra campos erroneos

--- view/src/components/landing/RegisterStep1.jsx
+++ view/src/components/landing/RegisterStep1.jsx
@@ -4,44 +4,39 @@ import debounce from "just-debounce-it"
 export function RegisterStep1({ setCurrentStep, setDivHeight, divHeight, setBusinessName, setTag, setPhoneNumber, setBusinessLocation, businessName, tag, phoneNumber, businessLocation }) {
     const [availableTag, setAvailableTag] = useState(false)
     const [tagProvisional, setTagProvisional] = useState('')
+    const [nameMistake, setNameMistake] = useState('')
+    const [tagMistake, setTagMistake] = useState('')
+    const [phoneMistake, setPhoneMistake] = useState('')
+    const [locationMistake, setLocationMistake] = useState('')
 
 
     const handleNameChange = (e) => {
+        setNameMistake('')
         setBusinessName(e.target.value)
     }
 
     const handleTagChange = (e) => {
+        setTagMistake('')
         setTagProvisional(e.target.value);
     }
 
     const handlePhoneChange = (e) => {
+        setPhoneMistake('')
         setPhoneNumber(e.target.value)
     }
 
     const handleLocationChange = (e) => {
+        setLocationMistake('')
         setBusinessLocation(e.target.value)
     }
 
     const handleRegister = () => {
-        if (businessName.trim() === '') {
-            alert('Business name cannot be empty')
-            return
-        }
+        setNameMistake(businessName.trim() === '' ? 'wrong' : '')
+        setTagMistake(tagProvisional.trim() === '' || !availableTag ? 'wrong' : '')
+        setPhoneMistake(phoneNumber.trim() === '' ? 'wrong' : '')
+        setLocationMistake(businessLocation.trim() === '' ? 'wrong' : '')
 
-        if (tag.trim() === '') {
-            alert('Tag cannot be empty')
-            return
-        }
-
-        if (phoneNumber.trim() === '') {
-            alert('Phone number cannot be empty')
-            return
-        }
-
-        if (businessLocation.trim() === '') {
-            alert('Business location cannot be empty')
-            return
-        }
+        if (businessName.trim() === '' || tagProvisional.trim() === '' || !availableTag || phoneNumber.trim() === '' || businessLocation.trim() === '') return
 
         setDivHeight("zero")
         setCurrentStep(2)
@@ -83,12 +78,12 @@ export function RegisterStep1({ setCurrentStep, setDivHeight, divHeight, setBusi
 
     return (
         <div className={`register-form ${divHeight}`}>
-            <input type="text" placeholder='business name' onInput={handleNameChange} className='register-input' value={businessName} />
+            <input type="text" placeholder='business name' onInput={handleNameChange} className={`register-input ${nameMistake}`} value={businessName} />
             <p className='register-tag-text'>The tag is a unique word that identifies your business publicly on forkkies. <span className={tagProvisional.length == 0 ? "not-available" : (availableTag ? "available" : "not-available")}>
                 {tagProvisional.length == 0 ? "Tag can't be empty" : (availableTag ? "This tag is available" : "This tag is in use")}</span></p>
-            <input type="text" placeholder='tag' onInput={handleTagChange} className='register-input' />
-            <input type="text" placeholder='phone number' onInput={handlePhoneChange} className='register-input' value={phoneNumber} />
-            <input type="text" placeholder='business location' onInput={handleLocationChange} className='register-input' value={businessLocation} />
+            <input type="text" placeholder='tag' onInput={handleTagChange} className={`register-input ${tagMistake}`} />
+            <input type="text" placeholder='phone number' onInput={handlePhoneChange} className={`register-input ${phoneMistake}`} value={phoneNumber} />
+            <input type="text" placeholder='business location' onInput={handleLocationChange} className={`register-input ${locationMistake}`} value={businessLocation} />
             <button className='register-button' onClick={handleRegister}>Next</button>
         </div>
     )
