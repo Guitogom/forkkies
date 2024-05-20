81a564ef6905dea3e1f69fa484fc446c16662923
Guitogom <guillemtg04@gmail.com>
2024-05-20 22:50:25 +0200

    [UPDATE] Cart finished

--- view/src/components/client/props/ClientOrderProduct.jsx
+++ view/src/components/client/props/ClientOrderProduct.jsx
@@ -54,12 +54,18 @@ export function ClientOrderProduct({ cartProduct, cart, setCart, secondaryColor,
         }
     }
 
+    const formatPrice = (price) => {
+        if (!price) return '0.00'
+        price = price.toFixed(2)
+        return price
+    }
+
     return (
         <div className="client-product-in-order">
             <img src={imagenDisplay} alt={cartProduct.name} />
             <div className="client-product-in-order-info">
-                <h3>{cartProduct.name}</h3>
-                <p>{cartProduct.price}</p>
+                <h3 style={{ color: secondaryColor }}>{cartProduct.name}</h3>
+                <p style={{ color: secondaryColor }}>{formatPrice(cartProduct.totalPrice)}€</p>
                 <div className='client-full-product-quantity-handler'>
                     <button className='client-full-product-add-to-cart-less' style={{ backgroundColor: secondaryColor, color: themeColor }} onClick={lessQuantity}>-</button>
                     <input type="number" value={innerCart.quantity} className='client-full-product-add-to-cart-input' style={{ backgroundColor: secondaryColor, color: themeColor }} onChange={placeQuantity} />
