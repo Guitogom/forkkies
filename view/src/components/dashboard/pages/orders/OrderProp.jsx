6e07ec66e0fc5f4a1c83c742e01bdab32c39af59
Guitogom <guillemtg04@gmail.com>
2024-05-24 18:09:13 +0200

    aa

--- view/src/components/dashboard/pages/orders/OrderProp.jsx
+++ view/src/components/dashboard/pages/orders/OrderProp.jsx
@@ -1,46 +1,18 @@
 import { PlaySVG } from "../../../../assets/svg/PlaySVG.jsx"
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate } from "react-router-dom"
 import { NextArrowSVG } from "../../../../assets/svg/NextArrowSVG.jsx"
 import { OrderDetailsProp } from "./OrderDetailsProp.jsx"
+import debounce from 'just-debounce-it'
 
 export function OrderProp({ order }) {
     const date = new Date(order.date).toLocaleString()
     const navigate = useNavigate()
     const [open, setOpen] = useState(false)
     const [status, setStatus] = useState(order.status)
-    const [swipeStatus, setSwipeStatus] = useState(null)
 
     const formatPrice = (price) => {
         return price % 1 === 0 ? `${price}.00` : price.toFixed(2)
-    }
-
-    const fetchOrders = () => {
-        if (localStorage.getItem('session_token') !== null) {
-            const token = localStorage.getItem('session_token')
-            fetch('https://api.forkkies.live/modifyorderstatus', {
-                method: 'GET',
-                headers: {
-                    'Authorization': `${token}`,
-                    'Content-Type': 'application/json'
-                },
-            })
-                .then(response => {
-                    if (!response.ok) {
-                        navigate('/error')
-                    }
-                    return response.json()
-                })
-                .then(orders => {
-                    setOrders(orders.result.orders)
-                    console.log(orders)
-                })
-                .catch(error => {
-                    console.error('Error:', error.message)
-                    localStorage.removeItem('session_token')
-                    navigate('/error')
-                })
-        }
     }
 
     const getStatusText = (status) => {
@@ -94,9 +66,37 @@ export function OrderProp({ order }) {
         }
     }
 
+    const fetchStatus = () => {
+        if (localStorage.getItem('session_token') !== null) {
+            const token = localStorage.getItem('session_token')
+            fetch('https://api.forkkies.live/modifyorderstatus', {
+                method: 'POST',
+                headers: {
+                    'Authorization': `${token}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ id: order.id, status: status })
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        navigate('/error')
+                    }
+                    return response.json()
+                })
+                .catch(error => {
+                    console.error('Error:', error.message)
+                    localStorage.removeItem('session_token')
+                    navigate('/error')
+                })
+        }
+    }
+
+    const debouncedFetchStatus = debounce(fetchStatus, 2000)
+
     const handleClick = () => {
         const nextStatus = getNextStatus(status)
         setStatus(nextStatus)
+        debouncedFetchStatus()
     }
 
     return (
