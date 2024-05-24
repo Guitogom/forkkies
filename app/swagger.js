d215153de3e42bc0e73d9d250854ef6b7598905b
pamukr <paumukros@gmail.com>
2024-05-24 09:26:28 +0200

    route corrected

--- app/swagger.js
+++ app/swagger.js
@@ -24,7 +24,7 @@ const specs = swaggerJsdoc(options);
 console.log('Swagger Docs generated:', specs);
 
 const swaggerDocs = (app) => {
-  app.use('/', swaggerUi.serve, swaggerUi.setup(specs));
+  app.use('/doc', swaggerUi.serve, swaggerUi.setup(specs));
 };
 
 export default swaggerDocs;
