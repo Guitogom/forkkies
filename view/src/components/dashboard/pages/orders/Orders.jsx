89fc448620d3c577937fe23705a545a1975220dd
Guitogom <guillemtg04@gmail.com>
2024-05-25 23:34:47 +0200

    Styles

--- view/src/components/dashboard/pages/orders/Orders.jsx
+++ view/src/components/dashboard/pages/orders/Orders.jsx
@@ -72,7 +72,7 @@ export function Orders({ business, orders, setOrders }) {
                     <p>Name</p>
                     <p>Total Price</p>
                     <p>Status</p>
-                    <button className={`order-refresh`} onClick={refreshOrders}><svg className={`${refreshing ? 'refreshing' : stopping ? 'stopping' : ''}`} fill="#000000" viewBox="0 0 24 24"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M4,12a1,1,0,0,1-2,0A9.983,9.983,0,0,1,18.242,4.206V2.758a1,1,0,1,1,2,0v4a1,1,0,0,1-1,1h-4a1,1,0,0,1,0-2h1.743A7.986,7.986,0,0,0,4,12Zm17-1a1,1,0,0,0-1,1A7.986,7.986,0,0,1,7.015,18.242H8.757a1,1,0,1,0,0-2h-4a1,1,0,0,0-1,1v4a1,1,0,0,0,2,0V19.794A9.984,9.984,0,0,0,22,12,1,1,0,0,0,21,11Z"></path></g></svg>Refresh</button>
+                    <button className={`order-refresh`} style={{ backgroundColor: `${refreshing ? 'var(--green)' : 'var(--forth-color)'}` }} onClick={refreshOrders}><svg className={`${refreshing ? 'refreshing' : stopping ? 'stopping' : ''}`} fill="#000000" viewBox="0 0 24 24"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M4,12a1,1,0,0,1-2,0A9.983,9.983,0,0,1,18.242,4.206V2.758a1,1,0,1,1,2,0v4a1,1,0,0,1-1,1h-4a1,1,0,0,1,0-2h1.743A7.986,7.986,0,0,0,4,12Zm17-1a1,1,0,0,0-1,1A7.986,7.986,0,0,1,7.015,18.242H8.757a1,1,0,1,0,0-2h-4a1,1,0,0,0-1,1v4a1,1,0,0,0,2,0V19.794A9.984,9.984,0,0,0,22,12,1,1,0,0,0,21,11Z"></path></g></svg>Refresh</button>
                 </div>
                 {orders.length > 0 ? (
                     orders.slice().reverse().map(order => (
