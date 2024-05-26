86e6be6c2aaeab6259b6976429dbd81622a5087d
pamukr <paumukros@gmail.com>
2024-05-26 15:06:58 +0200

    date format testing

--- view/src/components/dashboard/pages/orders/OrderProp.jsx
+++ view/src/components/dashboard/pages/orders/OrderProp.jsx
@@ -6,7 +6,8 @@ import { OrderDetailsProp } from "./OrderDetailsProp.jsx"
 import debounce from 'just-debounce-it'
 
 export function OrderProp({ order }) {
-    const date = new Date(order.date)
+    const date = order.date
+    // const newdate = new Date(order.date)
     // const hours = newdate.getUTCHours()
     // const minutes = newdate.getUTCMinutes()
     // const seconds = newdate.getUTCSeconds()
