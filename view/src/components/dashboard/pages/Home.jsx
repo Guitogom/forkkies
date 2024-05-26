e61d865be2a928fea5772571e0dac0b0db798d18
Guitogom <guillemtg04@gmail.com>
2024-05-26 15:22:03 +0200

    [ADD] Added QR

--- view/src/components/dashboard/pages/Home.jsx
+++ view/src/components/dashboard/pages/Home.jsx
@@ -1,15 +1,30 @@
+import { useRef } from 'react'
 import { Title } from "../Title"
 import '../../../styles/Home.css'
 import businessQR from '../../../assets/media/example_qr.png'
 import { Link } from 'react-router-dom'
-import {QRCode} from 'react-qrcode-logo'
+import { QRCode } from 'react-qrcode-logo'
 
 import { ManagementSVG } from '../../../assets/svg/ManagementSVG'
 import { ProductsSVG } from '../../../assets/svg/ProductsSVG'
 import { OrdersSVG } from "../../../assets/svg/OrdersSVG"
 import { AnalyticsSVG } from "../../../assets/svg/AnalyticsSVG"
 
-export function Home({ business, setBusiness }) {
+import LogoImage from '../../../assets/media/logo.png'
+
+export function Home({ business, setBusiness, primaryColor }) {
+
+    const canvasRef = useRef(null);
+
+    const handleDownload = () => {
+        const canvas = canvasRef.current.querySelector('canvas');
+        const url = canvas.toDataURL('image/png');
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = `${business.tag}Qr.png`;
+        link.click();
+    }
+
 
     return (
         <section>
@@ -22,8 +37,10 @@ export function Home({ business, setBusiness }) {
                     <Link to="/dashboard/analytics" className="box"><AnalyticsSVG /><h2>Analytics</h2></Link>
                 </aside>
                 <aside className="right-home">
-                    <p className="uppertext">Here is your preview</p>
-                    <QRCode value={business.name} />
+                    <p className="uppertext">Click the QR to download</p>
+                    <div ref={canvasRef} onClick={handleDownload}>
+                        <QRCode value={`https://forkkies.live/b/${business.tag}`} ecLevel="H" size='250' logoWidth='100' logoHeight='100' logoOpacity='0.5' qrStyle="dots" eyeRadius={10} bgColor="transparent" />
+                    </div>
                     <p className="undertext">Scan to preview</p>
                     {/* <button>Download</button> */}
                 </aside>
